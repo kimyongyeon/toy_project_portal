a2df04bcaefd75e89de3437f37ba245c6febd521
김용연 SKCC <jobkim0410@partner.sk.com>
2020-06-09 17:14:08 +0900

    [2020/06/09] 파일 업로드 틀 잡기

--- src/main/java/com/simple/portal/biz/v1/board/api/FileAPI.java
+++ src/main/java/com/simple/portal/biz/v1/board/api/FileAPI.java
@@ -1,11 +1,53 @@
 package com.simple.portal.biz.v1.board.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
+@RequestMapping("/v1/api")
 public class FileAPI {
 
     // todo: file upload
+    //Save the uploaded file to this folder
+    private static String UPLOADED_FOLDER = File.separator + "temp";
+
+    @PostMapping("/upload") // //new annotation since 4.3
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+
+        try {
+
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/uploadStatus";
+    }
+
+    @GetMapping("/uploadStatus")
+    public String uploadStatus() {
+        return "uploadStatus";
+    }
 
     // todo: file download link
 }
