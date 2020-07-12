100c694a6863c6c61cea3062376e129fab702e74
kimyongyeon <toyongyeon@gmail.com>
2020-07-12 07:38:58 +0000

    setting edit

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./mvnw clean package
+./mvnw clean package -Dmaven.test.skip=true
 docker build -t -f Dockerfile-dev toyongyeon/portalapp .
 docker run -p 8080:8080 toyongyeon/portalapp
