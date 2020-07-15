b39b8deb2105304a2a22c385e1ed9c17f5ef72fd
kimyongyeon <toyongyeon@gmail.com>
2020-07-15 22:45:35 +0900

    [2020/07/15] 게시판 기능 재정비

--- src/main/java/com/simple/portal/biz/v1/board/dto/CommentLikeDTO.java
+++ src/main/java/com/simple/portal/biz/v1/board/dto/CommentLikeDTO.java
@@ -7,13 +7,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class CommentLikeDTO {
-    @NotBlank(message="기본키는 필수 입력값입니다.")
+    @NotNull(message = "Please enter id")
     private Long id; // 아이디
     @NotBlank(message="아이템 구분은 필수 입력값입니다.")
     private String itemGb; // 아이템 구분 L:D
