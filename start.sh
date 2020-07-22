5dde2408f70933b57366533a8567e271eb4571df
kimyongyeon <toyongyeon@gmail.com>
2020-07-22 06:25:15 +0000

    start.sh commit

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+git pull origin release
 ./mvnw clean package -Dmaven.test.skip=true
-docker build -t -f Dockerfile-dev toyongyeon/portalapp .
-docker run -p 8080:8080 toyongyeon/portalapp
+docker build -t be-portal-app .
+docker stop be-portal-app
+docker rm be-portal-app
+docker run -it -d -p 8081:8081  \
+    -e spring.profiles.active="dev"  \
+    -e jasypt.encryptor.password="test"  \
+    --add-host=database:141.164.41.213 \
+    --name be-portal-app \
+    be-portal-app
