daa97f2524c1a3da696d9acb429525cccc2ba6c5
김용연 SKCC <jobkim0410@partner.sk.com>
2020-07-27 09:53:58 +0900

    [2020/07/27] Cross Origin check 풀림 - 2차

--- src/main/java/com/simple/portal/config/OkkyConfiguration.java
+++ src/main/java/com/simple/portal/config/OkkyConfiguration.java
@@ -5,13 +5,14 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableAutoConfiguration(exclude = { FreeMarkerAutoConfiguration.class })
-public class OkkyConfiguration {
+@EnableWebMvc
+public class OkkyConfiguration implements WebMvcConfigurer   {
 
     @Bean
     public ApiResponse apiResponse() { //  response bean
@@ -21,21 +22,10 @@ public class OkkyConfiguration {
         return apiResponse;
     }
 
-    @Bean
-    public WebMvcConfigurer webMvcConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods(HttpMethod.POST.name())
-                        .allowedMethods(HttpMethod.GET.name())
-                        .allowedMethods(HttpMethod.PUT.name())
-                        .allowedMethods(HttpMethod.DELETE.name())
-                        .allowCredentials(false)
-                        .maxAge(3600);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+
     }
 
 }
