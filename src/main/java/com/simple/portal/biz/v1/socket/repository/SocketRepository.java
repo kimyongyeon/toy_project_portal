66698a2db10920f4fc32ace28769ced820533e5d
kimyongyeon <toyongyeon@gmail.com>
2020-08-13 22:57:21 +0900

    [2020/08/13] 소켓쪽지함 테이블 변경

--- src/main/java/com/simple/portal/biz/v1/socket/repository/SocketRepository.java
+++ src/main/java/com/simple/portal/biz/v1/socket/repository/SocketRepository.java
@@ -1,8 +1,8 @@
 package com.simple.portal.biz.v1.socket.repository;
 
-import com.simple.portal.biz.v1.note.entity.NoteEntity;
+import com.simple.portal.biz.v1.socket.entity.SocketNoteEntitiy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SocketRepository extends JpaRepository<NoteEntity, Long> {
+public interface SocketRepository extends JpaRepository<SocketNoteEntitiy, Long> {
 
 }
