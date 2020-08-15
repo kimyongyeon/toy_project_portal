f8c8d57c9585d4a3058187ecaa33666a31a81dfd
kimyongyeon <toyongyeon@gmail.com>
2020-08-15 13:21:03 +0900

    Revert "Oauth 기능 추가 ( google, facebook, github, naver )"
    This reverts commit dcebe7de

--- src/main/java/com/simple/portal/config/SecurityConfig.java
+++ src/main/java/com/simple/portal/config/SecurityConfig.java
@@ -1,20 +1,14 @@
 package com.simple.portal.config;
 
-import com.simple.portal.biz.v1.user.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-
-import static com.simple.portal.biz.v1.user.security.SocialType.*;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
@@ -37,27 +31,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and()
             .csrf()
             .disable()// rest api이므로 csrf 보안이 필요없으므로 disable처리.
-            .authorizeRequests().antMatchers("/", "/oauth2/**", "/login/**", "/css/**", "/images/**", "/js/**", "/console/**", "/favicon.ico/**") // // url 별 권한 관리를 설정하는 옵션의 시작점. 이게 선언되어야 antMatchers  옵션을 사용할 수 있음.
-                .permitAll()  // 위에 있는 경로는 누구나 접근 가능
-                .antMatchers("/google").hasAuthority(GOOGLE.getRoleType()) // 관리 대상을 지정하는 옵션. google, facebook, naver는 role을 가지고 있어야만 접근가능. 즉, 인증된 사용자만 가능
-                .antMatchers("/facebook").hasAuthority(FACEBOOK.getRoleType())
-                .antMatchers("/naver").hasAuthority(NAVER.getRoleType())
-                .antMatchers("/github").hasAnyAuthority(GITHUB.getRoleType())
+            .authorizeRequests()
             .antMatchers("/v1/api/**", "/h2-console/**").permitAll()
             .antMatchers("/user").hasAuthority("USER")
             .antMatchers("/admin").hasAuthority("ADMIN")
-            .anyRequest().authenticated()// anyRequest( )는 위에서 설정한 것 외의 나머지 경로를 뜻함. authenticated() 메소드를 호출하여 인증된 사용자만 접근 하도록 함. ( 로그인된 사용자 )
-            .and().oauth2Login()
-                .userInfoEndpoint().userService(new CustomOAuth2UserService())
-                .and()
-                .defaultSuccessUrl("/loginSuccess")// 성공시 redirect 될 url
-                .failureUrl("/loginFailure")// 실패시 redirect 될 url
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"));
-        //
-        //       .logout();
-        //  http.headers().frameOptions().disable();
+            .anyRequest().authenticated()
+            .and()
+    //               .formLogin()
+    //                    .and()
+            .logout()
+        ;
+        http.headers().frameOptions().disable();
+
     }
 
     @Override
@@ -68,11 +53,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "/websocket", "/sockjs","/stomp", "/websockethandler/**", "/webjars/**", "/greetings/**", "/ws-stomp/**");
     }
 
-    /*
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-    */
 }
