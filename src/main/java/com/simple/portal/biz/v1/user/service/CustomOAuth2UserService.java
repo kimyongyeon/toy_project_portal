3285baf13f2da6b294b04e5a6ba9b2317433cc06
xowns4817 <xowns4817@naver.com>
2020-08-18 16:24:46 +0900

    oauth return 값에 platform 추가

--- src/main/java/com/simple/portal/biz/v1/user/service/CustomOAuth2UserService.java
+++ src/main/java/com/simple/portal/biz/v1/user/service/CustomOAuth2UserService.java
@@ -1,7 +1,10 @@
 package com.simple.portal.biz.v1.user.service;
 
 import com.simple.portal.biz.v1.user.dto.OAuthAttributes;
+import com.simple.portal.biz.v1.user.repository.UserRepository;
+import com.simple.portal.common.Interceptor.JwtUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -13,11 +16,14 @@ import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 @Slf4j
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
@@ -30,11 +36,16 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
         OAuthAttributes attributes = OAuthAttributes.
                 of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
-
+        /*
         log.info("nickname : " + attributes.getName());
         log.info("email : " +  attributes.getEmail());
         log.info("profile : " + attributes.getPicture());
+        log.info("platform : " + attributes.getPlatform());
         log.info("full data : " + attributes.getAttributes());
+        */
+
+        Map<String, Object> attributesWithPlatform = new LinkedHashMap<>(attributes.getAttributes());
+        attributesWithPlatform.put("platform", registrationId);
 
         Set<GrantedAuthority> authorities = new LinkedHashSet<>();
         authorities.add(new OAuth2UserAuthority(attributes.getAttributes()));
@@ -45,7 +56,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
         return new DefaultOAuth2User(
                 authorities,
-                attributes.getAttributes(),
+                attributesWithPlatform,
                 attributes.getNameAttributeKey());
     }
 }
