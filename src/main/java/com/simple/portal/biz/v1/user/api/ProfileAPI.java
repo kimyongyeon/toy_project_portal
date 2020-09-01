3fe642fff3d79853be7974645817c466e0deaeb5
xowns4817 <xowns4817@naver.com>
2020-09-01 22:59:46 +0900

    [태준] file-api cors 추가

--- src/main/java/com/simple/portal/biz/v1/user/api/ProfileAPI.java
+++ src/main/java/com/simple/portal/biz/v1/user/api/ProfileAPI.java
@@ -8,10 +8,7 @@ import com.simple.portal.common.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
@@ -20,6 +17,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/v1/api")
 @Slf4j
+@CrossOrigin
 public class ProfileAPI {
 
     private ApiResponse apiResponse;
