2929fe7f0a34370713bea158ae1fcc32dcae9deb
kimyongyeon <toyongyeon@gmail.com>
2020-09-05 20:39:36 +0900

    Revert "[09/05] websocket 정리 및 stomp.html 예제 수정"
    This reverts commit d19d193f

---
+++ src/main/java/com/simple/portal/biz/v1/socket/entity/SocketEntity.java
@@ -0,0 +1,13 @@
+package com.simple.portal.biz.v1.socket.entity;
+
+import lombok.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class SocketEntity {
+    private int notReadCount;
+    //private String msg;
+}
